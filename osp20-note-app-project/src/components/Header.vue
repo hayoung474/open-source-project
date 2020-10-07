50a689a720c15dd6d3e6d7565bfc2fb1af99cd52
hayoungzzzzz <hayoung474@gmail.com>
2020-10-07 14:42:15 +0900

    vuetify 추가

---
+++ osp20-note-app-project/src/components/Header.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="header">     
+      <img src="../assets/logo.png">
+      <p>Vue.JS Sticky Notes</p>
+      <v-btn class="add-btn" @click.prevent="openEditor"><i class="fas fa-plus"></i></v-btn>    
+    </div>
+</template>
+
+<script>
+    export default {
+       methods:{
+          openEditor: function(){
+            this.$emit('openEditor');
+          }
+        }
+    }
+</script>
