f8b6fa62089ca01525dd558a9189152a757717ee
hayoungzzzzz <hayoung474@gmail.com>
2020-10-07 18:33:35 +0900

    이전 커밋과 동일

--- osp20-note-app-project/src/App.vue
+++ osp20-note-app-project/src/App.vue
@@ -1,85 +1,100 @@
 <template>
-  <div id="app">
-    <app-header></app-header>
-    <app-note-editor
-      v-if="editorOpen"
-      @noteAdded="newNote"
-      @noteDeleted="deleteNote"
-    ></app-note-editor>
-    <div class="noteContainer">
-      <div
-        v-for="(note, index) in notes"
-        :key="`note-${index}`"
-        class="note"
-        :style="{ 'background-color': note.theme }"
-      >
-        <div>
-          <span class="delete" @click.prevent="deleteNote(index)"
-            ><i class="fas fa-times"></i
-          ></span>
-          <span>{{ note.title }}</span>
-          <p class="note-text">{{ note.text }}</p>
-        </div>
-      </div>
-      <v-btn class="mx-2 add-button" fab dark color="Indigo" @click="editorOpen = !editorOpen">
-        <v-icon dark> mdi-plus </v-icon>
-      </v-btn>
+    <div id="app">
+        <v-app id="inspire">
+            <app-header></app-header>
+            <div class="noteContainer">
+                <div
+                    v-for="(note, index) in notes"
+                    :key="`note-${index}`"
+                    class="note"
+                    :style="{ 'background-color': note.theme }">
+                    <div>
+                        <span class="delete" @click.prevent="deleteNote(index)">
+                            <i class="fas fa-times"></i >
+                        </span>
+                        <span>{{ note.title }}</span>
+                        <p class="note-text">{{ note.text }}</p>
+                    </div>
+                </div>
+                <v-btn
+                    class="mx-2 add-button"
+                    fab="fab"
+                    dark="dark"
+                    color="Indigo"
+                    @click="dialog = true">
+                    <v-icon dark="dark">
+                        mdi-plus
+                    </v-icon>
+                </v-btn>
+                
+                <v-dialog v-model="dialog" max-width="800" color="white">
+                    <app-note-editor @noteAdded="newNote" @noteDeleted="deleteNote"></app-note-editor>
+                </v-dialog>
+
+            </div>
+        </v-app>
     </div>
-  </div>
 </template>
 
 <script>
-import NoteEditor from "./components/NoteEditor.vue";
-import Header from "./components/Header.vue";
+    import NoteEditor from "./components/NoteEditor.vue";
+    import Header from "./components/Header.vue";
 
-export default {
-  name: "App",
-  data: function () {
-    return {
-      editorOpen: false,
-      notes: [
-        {
-          title: "Code",
-          text: "1131111222",
-          theme: "#FF8A80",
+    export default {
+        name: "App",
+        data: function () {
+            return {
+                dialog: false,
+                editorOpen: false,
+                notes: [
+                    {
+                        title: "Code",
+                        text: "1131111222",
+                        theme: "#FF8A80"
+                    }, {
+                        title: "event",
+                        text: "event",
+                        theme: "#DDA0DD"
+                    }
+                ]
+            };
         },
-        {
-          title: "event",
-          text: "event",
-          theme: "#DDA0DD",
+        computed: {},
+        methods: {
+            newNote(title, text, theme, CloseEditor) {
+                this.dialog = CloseEditor;
+                console.log(CloseEditor);
+                this
+                    .notes
+                    .push({title: title, text: text, theme: theme});
+            },
+            deleteNote(index) {
+                this
+                    .notes
+                    .splice(index, 1);
+            }
         },
-      ],
+        mounted() {
+            if (localStorage.getItem("notes")) 
+                this.notes = JSON.parse(localStorage.getItem("notes"));
+            }
+        ,
+        watch: {
+            notes: {
+                handler() {
+                    var newNotes = this.notes;
+                    localStorage.setItem("notes", JSON.stringify(newNotes));
+                },
+                deep: true
+            }
+        },
+        components: {
+            appNoteEditor: NoteEditor,
+            appHeader: Header
+        }
     };
-  },
-  computed: {},
-  methods: {
-    newNote(title, text, theme) {
-      this.notes.push({ title: title, text: text, theme: theme });
-    },
-    deleteNote(index) {
-      this.notes.splice(index, 1);
-    },
-  },
-  mounted() {
-    if (localStorage.getItem("notes"))
-      this.notes = JSON.parse(localStorage.getItem("notes"));
-  },
-  watch: {
-    notes: {
-      handler() {
-        var newNotes = this.notes;
-        localStorage.setItem("notes", JSON.stringify(newNotes));
-      },
-      deep: true,
-    },
-  },
-  components: {
-    appNoteEditor: NoteEditor,
-    appHeader: Header,
-  },
-};
 </script>
 
 <style lang="scss">
-@import "@/styles/global.scss";
+    @import "@/styles/global.scss";
 </style>
--- osp20-note-app-project/src/components/NoteEditor.vue
+++ osp20-note-app-project/src/components/NoteEditor.vue
@@ -1,39 +1,138 @@
 <template>
-<div>
-    <div class="note-grid">
-        <div class="note-editor">
-            <input class="title-input" type="text" v-model="title" placeholder="Title">
-            <textarea rows="10" v-model="text" placeholder="Take a note..."></textarea>           
-            <div class="note-editor-bottom">
-                <button @click=createNew class="fas fas-check-circle"><i class="fas fa-check-circle"></i></button>
-            </div>  
-           
+    <div>
+        <v-container>
+            <div>
+                <v-card solo="solo">
+                    <div class="ma-5">
+                        <v-row >
+                            <v-col cols="12" md="6">
+                                <v-card-title>Input Note</v-card-title>
+                                <v-divider></v-divider>
+                                <div class="note-editor mt-5">
+                                    <input class="title-input" type="text" v-model="title" placeholder="Title">
+                                        <textarea rows="10" v-model="text" placeholder="Take a note..."></textarea>
+
+                                    </div>
+                                </v-col>
+                                <v-col cols="12" md="6">
+                                    <v-card-title>Color Pick</v-card-title>
+                                    <v-divider></v-divider>
+                                    <div style="text-align: -webkit-center;">
+                                        <v-color-picker justify="center" v-model="theme" class="mt-5"></v-color-picker>
+                                    </div>
+                                </v-col>
+                            </v-row>
+                            <v-card-title>Options</v-card-title>
+                            <v-divider></v-divider>
+                            <v-row>
+                                <v-col cols="4">
+                                    <div class="text-center">
+                                        <v-menu offset-y="offset-y">
+                                            <template v-slot:activator="{ on, attrs }">
+                                                <v-btn text="text" color="black" v-bind="attrs" v-on="on" outlined="outlined">
+                                                    Category
+                                                </v-btn>
+                                                <p>{{category}}</p>
+                                            </template>
+                                            <v-list>
+                                                <v-list-item-group v-model="category">
+                                                    <v-list-item v-for="(item, index) in items" :key="index" link="link">
+                                                        <v-list-item-title >
+                                                            {{ item.title }}</v-list-item-title>
+                                                    </v-list-item>
+                                                </v-list-item-group>
+                                            </v-list>
+                                        </v-menu>
+                                    </div>
+                                </v-col>
+                                <v-col cols="4">
+                                    <v-checkbox
+                                        label="Secret Memo"
+                                        color="black"
+                                        v-model="secret"
+                                        hide-details="hide-details"></v-checkbox>
+                                </v-col>
+                                <v-col cols="4">
+                                    <v-checkbox
+                                        label="Important Memo"
+                                        color="black"
+                                        v-model="important"
+                                        hide-details="hide-details"></v-checkbox>
+                                </v-col>
+
+                            </v-row>
+                            <v-divider></v-divider>
+                            <v-row>
+                                <v-spacer></v-spacer>
+                                <v-col cols="2">
+                                    <div class="note-editor-bottom">
+
+                                        <v-btn
+                                            text="text"
+                                            color="black"
+                                            class="write-btn"
+                                            @click="createNew"
+                                            outlined="outlined">WRITE</v-btn>
+                                    </div>
+                                </v-col>
+                            </v-row>
+                        </div>
+                    </v-card>
+                </div>
+
+            </v-container>
         </div>
-    </div>
-     <v-color-picker v-model="theme"></v-color-picker>
-    </div>
 </template>
+    <script>
 
-<script>
+        export default {
+            data() {
+                return {
+                    title: '',
+                    theme: '',
+                    text: '',
+                    dialog: false,
+                    secret: false,
+                    important: false,
+                    category: '',
 
-export default {
-    data: function() {
-        return {
-            title: '',            
-            theme: '',            
-            text: ''
+                    items: [
+                        {
+                            title: 'aaa'
+                        }, {
+                            title: 'bbb'
+                        }, {
+                            title: 'ccc'
+                        }, {
+                            title: 'ddd'
+                        }
+                    ]
+                }
+            },
+            methods: {
+                createNew() {
+                    this.dialog = false;
+                    console.log(this.category);
+                    this.$emit('noteAdded', this.title, this.text, this.theme, this.dialog);
+                    this.title = '';
+                    this.text = '',
+                    this.theme = '';
+                },
+                deleteNote(index) {
+                    this.$emit('noteDeleted', index);
+                }
+            }
         }
-    },   
-    methods: {        
-        createNew() {
-            this.$emit('noteAdded', this.title, this.text, this.theme);
-            this.title = '';
-            this.text = '',
-            this.theme = '';
-        },
-        deleteNote(index) {
-            this.$emit('noteDeleted', index);
-        },
-    },
-}
-</script>
+    </script>
+    <style>
+        .write-btn {
+            margin: 10px;
+        }
+        .tag-input {
+            margin-left: 10px;
+        }
+        .v-input--selection-controls {
+            margin-top: 5px !important;
+            padding: 0 !important;
+        }
+    </style>
