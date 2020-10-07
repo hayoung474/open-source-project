7bc30d35541e23f1d98647bf2491d33b79194e89
20181125 <oo_303@naver.com>
2020-10-07 18:34:10 +0900

    검색바 만들기

---
+++ osp20-note-app-project/src/components/SearchBar.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="search-container">
+    <input
+      type="text"
+      v-model="searchText"
+      name="search"
+      placeholder="Search..."
+      class="search-input"
+    />
+    <a href="#" class="search-btn">
+      <i class="fas fa-search"></i>
+    </a>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      searchText: "",
+    };
+  },
+};
+</script>
