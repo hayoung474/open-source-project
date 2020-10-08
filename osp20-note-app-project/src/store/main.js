341a52b20419ed105cc867285272f013e9cf0ff9
hayoungzzzzz <hayoung474@gmail.com>
2020-10-09 02:23:16 +0900

    전역 변수 사용을 위한 Vuex 모듈 설치

---
+++ osp20-note-app-project/src/store/main.js
@@ -0,0 +1,21 @@
+import Vue from 'vue'
+import App from './App.vue'
+import Vuex from 'vuex'
+Vue.use(Vuex)
+
+const store = new Vuex.Store({
+    state:{
+
+    },
+    mutations:{
+
+    }
+})
+
+Vue.config.productionTip = false
+
+new Vue({
+    store,
+    render: h => h(App),
+
+}).$mount('#app')
