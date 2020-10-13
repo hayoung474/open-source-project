e12d39eae03f19caf57e15307e491e9032f101ea
20181125 <oo_303@naver.com>
2020-10-13 19:51:17 +0900

    비밀번호 입력창 구현

--- osp20-note-app-project/src/components/NoteEditor.vue
+++ osp20-note-app-project/src/components/NoteEditor.vue
@@ -1,99 +1,135 @@
 <template>
-    <div>
-        <v-container>
-            <div class="dialog-div">
-                <v-card solo="solo">
-                    <div class="dialog-div2">
-                        <v-row>
-                            <v-col cols="12" md="6">
-                                <v-card-title>Input Note</v-card-title>
-                                <v-divider></v-divider>
-                                <div class="note-editor mt-5">
-                                    <input class="title-input" type="text" v-model="title" placeholder="Title">
-                                        <textarea rows="10" v-model="text" placeholder="Take a note..."></textarea>
-                                    </div>
-                                </v-col>
-                                <v-col cols="12" md="6">
-                                    <v-card-title>Color Pick</v-card-title>
-                                    <v-divider></v-divider>
-                                    <div style="text-align: -webkit-center;">
-                                        <v-color-picker justify="center" v-model="theme" class="mt-5"></v-color-picker>
-                                    </div>
-                                </v-col>
-                            </v-row>
-                            <v-card-title>Options</v-card-title>
-                            <v-divider></v-divider>
-                            <v-row>
-                                <v-col cols="4">
-                                    <div class="text-center">
-                                        <v-menu offset-y="offset-y">
-                                            <template v-slot:activator="{ on, attrs }">
-                                                <v-btn text="text" color="black" v-bind="attrs" v-on="on" outlined="outlined">
-                                                    Category
-                                                </v-btn>
-                                                <p>{{selectCategoryName}}</p>
-                                            </template>
-                                            <v-list>
-                                                <v-list-item-group v-model="select">
-                                                    <v-list-item v-for="(item, index) in category" :key="index" link="link">
-                                                        <v-list-item-title >
-                                                            {{ item.title }}</v-list-item-title>
-                                                    </v-list-item>
-                                                </v-list-item-group>
-                                            </v-list>
-                                        </v-menu>
-                                    </div>
-                                </v-col>
-                                <v-col cols="4">
-                                    <v-checkbox
-                                        label="Secret Memo"
-                                        color="black"
-                                        v-model="secret"
-                                        hide-details="hide-details"></v-checkbox>
-                                </v-col>
-                                <v-col cols="4">
-                                    <v-checkbox
-                                        label="Important Memo"
-                                        color="black"
-                                        v-model="important"
-                                        hide-details="hide-details"></v-checkbox>
-                                </v-col>
-
-                            </v-row>
-                            <v-divider></v-divider>
-                            <v-row>
-                                <v-spacer></v-spacer>
-                                <v-col cols="5">
-                                    <div class="note-editor-bottom">
-                                        <span>
-                                            <v-btn
-                                                text="text"
-                                                color="black"
-                                                class="write-btn"
-                                                @click="cancel"
-                                                outlined="outlined">CANCEL</v-btn>
-                                        </span>
-                                        <span>
-                                            <v-btn
-                                                text="text"
-                                                color="black"
-                                                class="write-btn"
-                                                @click="createNew"
-                                                outlined="outlined">WRITE</v-btn>
-                                        </span>
-                                    </div>
-                                </v-col>
-                            </v-row>
-                        </div>
-                    </v-card>
+  <div>
+    <v-container>
+      <div class="dialog-div">
+        <v-card solo="solo">
+          <div class="dialog-div2">
+            <v-row>
+              <v-col cols="12" md="6">
+                <v-card-title>Input Note</v-card-title>
+                <v-divider></v-divider>
+                <div class="note-editor mt-5">
+                  <input
+                    class="title-input"
+                    type="text"
+                    v-model="title"
+                    placeholder="Title"
+                  />
+                  <textarea
+                    rows="10"
+                    v-model="text"
+                    placeholder="Take a note..."
+                  ></textarea>
                 </div>
-            </v-container>
-        </div>
+              </v-col>
+              <v-col cols="12" md="6">
+                <v-card-title>Color Pick</v-card-title>
+                <v-divider></v-divider>
+                <div style="text-align: -webkit-center">
+                  <v-color-picker
+                    justify="center"
+                    v-model="theme"
+                    class="mt-5"
+                  ></v-color-picker>
+                </div>
+              </v-col>
+            </v-row>
+            <v-card-title>Options</v-card-title>
+            <v-divider></v-divider>
+            <v-row>
+              <v-col cols="4">
+                <div class="text-center">
+                  <v-menu offset-y="offset-y">
+                    <template v-slot:activator="{ on, attrs }">
+                      <v-btn
+                        text="text"
+                        color="black"
+                        v-bind="attrs"
+                        v-on="on"
+                        outlined="outlined"
+                      >
+                        Category
+                      </v-btn>
+                      <p>{{ selectCategoryName }}</p>
+                    </template>
+                    <v-list>
+                      <v-list-item-group v-model="select">
+                        <v-list-item
+                          v-for="(item, index) in category"
+                          :key="index"
+                          link="link"
+                        >
+                          <v-list-item-title>
+                            {{ item.title }}</v-list-item-title
+                          >
+                        </v-list-item>
+                      </v-list-item-group>
+                    </v-list>
+                  </v-menu>
+                </div>
+              </v-col>
+              <v-col cols="4">
+                <v-checkbox
+                  label="Secret Memo"
+                  color="black"
+                  v-model="secret"
+                  hide-details="hide-details"
+                ></v-checkbox>
+                <input
+                  v-if="this.secret"
+                  class="password-input"
+                  type="text"
+                  v-model="password"
+                  placeholder="password"
+                />
+              </v-col>
+              <v-col cols="4">
+                <v-checkbox
+                  label="Important Memo"
+                  color="black"
+                  v-model="important"
+                  hide-details="hide-details"
+                ></v-checkbox>
+              </v-col>
+            </v-row>
+            <v-divider></v-divider>
+            <v-row>
+              <v-spacer></v-spacer>
+              <v-col cols="5">
+                <div class="note-editor-bottom">
+                  <span>
+                    <v-btn
+                      text="text"
+                      color="black"
+                      class="write-btn"
+                      @click="cancel"
+                      outlined="outlined"
+                      >CANCEL</v-btn
+                    >
+                  </span>
+                  <span>
+                    <v-btn
+                      text="text"
+                      color="black"
+                      class="write-btn"
+                      @click="createNew"
+                      outlined="outlined"
+                      >WRITE</v-btn
+                    >
+                  </span>
+                </div>
+              </v-col>
+            </v-row>
+          </div>
+        </v-card>
+      </div>
+    </v-container>
+  </div>
 </template>
 <script>
 export default {
   props: {
-    modifyIndex:String
+    modifyIndex: String,
   },
   data() {
     return {
@@ -102,17 +138,17 @@ export default {
       text: "",
       secret: false,
       important: false,
+      password: "",
 
       category: [],
       select: 0,
       selectCategoryName: "",
 
       dialog: false,
-
     };
   },
   // created() {
-        
+
   //       this.$store.state.category = JSON.parse(localStorage.getItem("category"));
   //       this.category = this.$store.state.category;
 
@@ -121,7 +157,7 @@ export default {
     this.category = this.$store.state.category;
     this.selectCategoryName = this.category[this.select].title;
   },
-  
+
   methods: {
     createNew() {
       if (this.title == "") {
@@ -133,33 +169,37 @@ export default {
       if (this.title != "" && this.text != "") {
         this.dialog = false;
         var note = {
-            title: this.title,
-            text: this.text,
-            theme: this.theme,
-            date: new Date().toISOString().substr(0, 10) + " "+new Date().toTimeString().substr(0,8),
-            sortDate : new Date(),
-            category: this.category[this.select],
-            secret: this.secret,
-            important: this.important
+          title: this.title,
+          text: this.text,
+          theme: this.theme,
+          date:
+            new Date().toISOString().substr(0, 10) +
+            " " +
+            new Date().toTimeString().substr(0, 8),
+          sortDate: new Date(),
+          category: this.category[this.select],
+          secret: this.secret,
+          important: this.important,
         };
         // 노트 추가
         this.noteAdd(note);
 
-        this.title="";
-        this.text="";
-        this.theme="";
-        this.category=null;
-        this.secret=false;
-        this.important=false;
+        this.title = "";
+        this.text = "";
+        this.theme = "";
+        this.category = null;
+        this.secret = false;
+        this.important = false;
+        this.password = "";
       }
     },
     noteAdd(note) {
-        this.$emit("editorClose");
-        this.$store.commit("addNote",note);
+      this.$emit("editorClose");
+      this.$store.commit("addNote", note);
     },
-    cancel(){
-        this.$emit("editorClose");
-    }
+    cancel() {
+      this.$emit("editorClose");
+    },
   },
 
   // mounted() {
@@ -177,16 +217,16 @@ export default {
       },
       deep: true,
     },
-    modifyIndex:{
-        handler(){
-            this.index = this.modifyIndex;
-            this.title = this.$store.state.notes[this.modifyIndex].title;
-            this.text = this.$store.state.notes[this.modifyIndex].text;
-            this.theme = this.$store.state.notes[this.modifyIndex].theme;
-            this.secret = this.$store.state.notes[this.modifyIndex].secret;
-            this.important = this.$store.state.notes[this.modifyIndex].important;
-        }
-    }
+    modifyIndex: {
+      handler() {
+        this.index = this.modifyIndex;
+        this.title = this.$store.state.notes[this.modifyIndex].title;
+        this.text = this.$store.state.notes[this.modifyIndex].text;
+        this.theme = this.$store.state.notes[this.modifyIndex].theme;
+        this.secret = this.$store.state.notes[this.modifyIndex].secret;
+        this.important = this.$store.state.notes[this.modifyIndex].important;
+      },
+    },
   },
 };
 </script>
